367922a697b91ebd0a42155be70f96986c07f5a4
Ivan Godoy-Smirnov <iagsmirnov@gmail.com>
2021-05-05 15:03:15 -0400

    found a library and it works well so far. The weights are stored as lazily-loaded julia artifacts (introduced in Julia 1.3).

--- practice.jl
+++ practice.jl
@@ -1,5 +1,20 @@
-print("Hello world")
+using Pkg
+Pkg.add("ObjectDetector")
+Pkg.add("FileIO")
+using ObjectDetector, FileIO
 
-while (true)
-	sleep(1)
-end
+yolomod = YOLO.v3_tiny_COCO() # Load the YOLOv3-tiny model pretrained on COCO, with a batch size of 1
+
+batch = emptybatch(yolomod) # Create a batch object. Automatically uses the GPU if available
+
+img = load(joinpath(dirname(dirname(pathof(ObjectDetector))),"test","images","dog-cycle-car.png"))
+
+batch[:,:,:,1], padding = prepareImage(img, yolomod) # Send resized image to the batch
+
+res = yolomod(batch, detectThresh=0.5, overlapThresh=0.5) # Run the model on the length-1 batch
+
+imgBoxes = drawBoxes(img, yolomod, padding, res)
+save("results.png", imgBoxes)
+
+print("Hello World")
+
